99b03e41549ce299e3f7b6fe929516d4ebb3c7c9
Nitheesh Krishna <nitheeshhs@gmail.com>
2022-11-06 10:26:53 +0530

    reverse-priority-queue

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,104 +1,42 @@
-/**
- ** Java Program to implement Priority Queue
- **/
+import java.util.*;
 
-/** class Task **/
-class Level
-{
-    int level;
-    int range;
+public class PriorityQueueDemo {
 
-
-    /** Constructor **/
-    public Level(int level, int range)
+    // Main Method
+    public static void main(String[] args)
     {
-        this.level = level;
-        this.range = range;
+        // Creating empty priority queue
+        // with custom Comparator
+        PriorityQueue<Integer> pQueue
+                = new PriorityQueue<Integer>(
+                new Comparator<Integer>() {
+
+                    // Compare method for place element in
+                    // reverse order
+                    public int compare(Integer a, Integer b)
+                    {
+                        if (a < b)
+                            return 1;
+                        if (a > b)
+                            return -1;
+                        return 0;
+                    }
+                });
+
+        // Adding items to the pQueue using add()
+        pQueue.add(10);
+        pQueue.add(15);
+        pQueue.add(20);
+        pQueue.add(5);
+
+        // Printing the top element of PriorityQueue
+        System.out.println(pQueue.peek());
+
+        // Printing the top element and removing it
+        // from the PriorityQueue container
+        System.out.println(pQueue.poll());
+
+        // Printing the top element again
+        System.out.println(pQueue.peek());
     }
-
-    public String toString()
-    {
-        return "Level : "+ level +"\nPriority : "+ range;
-    }
-}
-
-class PriorityQueue
-{
-    private Level[] heap;
-    private int heapSize, capacity;
-
-
-    /** Constructor **/
-    public PriorityQueue(int capacity)
-    {
-        this.capacity = capacity + 1;
-        heap = new Level[this.capacity];
-        heapSize = 0;
-    }
-    public void clear()
-    {
-        heap = new Level[capacity];
-        heapSize = 0;
-    }
-    public boolean isEmpty()
-    {
-        return heapSize == 0;
-    }
-
-   public boolean isFull()
-    {
-        return heapSize == capacity - 1;
-    }
-    /** function to get Size **/
-    public int size()
-    {
-        return heapSize;
-    }
-    /** function to insert task **/
-    public void insert(int level, int range)
-    {
-        Level newJob = new Level(level, range);
-
-        heap[++heapSize] = newJob;
-        int pos = heapSize;
-        while (pos != 1 && newJob.range > heap[pos/2].range)
-        {
-            heap[pos] = heap[pos/2];
-            pos /=2;
-        }
-        heap[pos] = newJob;
-    }
-    /** function to remove task **/
-    public Level remove()
-    {
-        int parent, child;
-        Level item, temp;
-        if (isEmpty() )
-        {
-            System.out.println("Heap is empty");
-            return null;
-        }
-
-        item = heap[1];
-        temp = heap[heapSize--];
-
-        parent = 1;
-        child = 2;
-        while (child <= heapSize)
-        {
-            if (child < heapSize && heap[child].range < heap[child + 1].range)
-                child++;
-            if (temp.range >= heap[child].range)
-                break;
-
-            heap[parent] = heap[child];
-            parent = child;
-            child *= 2;
-        }
-        heap[parent] = temp;
-
-        return item;
-    }
-
-
 }
